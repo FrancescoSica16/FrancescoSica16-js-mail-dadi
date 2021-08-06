7bdcd7b9602abda2e95c49a0a27dc047e64ded1c
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-06 15:55:11 +0200

    start

---
+++ myscript.js
@@ -0,0 +1,23 @@
+
+const emailList = ["pluto@gmail.com", "pippo@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "bugo@gmail.com"];
+
+console.log(emailList);
+
+let emailInserita = prompt("Inserisci la tua email per accedere");
+
+
+for ( i=0 ; i < emailList.length ; i++)
+
+    if (emailInserita = emailList) {
+
+        console.log("puoi entrare");
+    }
+
+    else (emailInserita =! emailList) {
+
+        console.log("non puoi entrare") 
+    }
+
+
+
+
