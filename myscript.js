033fa7b2efbf8529942f8f44b27efb7310fd3ef9
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-25 09:43:09 +0200

    lancio dadi

--- myscript.js
+++ myscript.js
@@ -1,22 +1,47 @@
 
-const emailList = ["pluto@gmail.com", "pippo@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "bugo@gmail.com"];
-
-console.log(emailList);
+let emailList = ["pluto@gmail.com", "pippo@gmail.com", "topolino@gmail.com", "paperino@gmail.com", "bugo@gmail.com"];
 
 let emailInserita = prompt("Inserisci la tua email per accedere");
+let emailConf = false;
 
+for ( i=0 ; i < emailList.length ; i++){
 
-for ( i=0 ; i < emailList.length ; i++)
-
-    if (emailInserita = emailList) {
-
-        console.log("puoi entrare");
+    if (emailInserita == emailList[i]) {      
+        emailConf = true;       
     }
+      
+} 
+   if (emailConf == true) {
+    document.getElementById("login").innerHTML = emailInserita + " puoi entrare";
+   }
 
-    else (emailInserita =! emailList) {
+   else {
+    document.getElementById("login").innerHTML = emailInserita  + " non puoi entrare"; 
+   }
+      
+   
+const dado = ['1', '2', '3', '4', '5', '6'];
+console.log(dado);
 
-        console.log("non puoi entrare") 
-    }
+console.log(dado.length);
+
+let numUser = parseInt(Math.random() * dado.length+1);
+let numComputer = parseInt(Math.random() * dado.length+1);
+console.log("il numero scelto dal pc per te è" + " " + numUser);
+console.log("il numero scelto dal pc è" + " " + numComputer);
+
+document.getElementById("user").innerHTML = "in numero scelto per te è " + numUser;
+document.getElementById("pc").innerHTML =   "in numero scelto dal pc è " + numComputer; 
+
+if(numUser < numComputer){
+    document.getElementById("result").innerHTML = "hai perso";
+
+} else if (numUser == numComputer){
+    document.getElementById("result").innerHTML = "siete pari";
+} else {
+    document.getElementById("result").innerHTML = "hai vinto";
+
+}
 
 
 
